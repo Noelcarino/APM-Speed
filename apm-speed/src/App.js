93b6dc0b3ea6b0fdaa5e18738ad3d7e7ed3b7849
Noelcarino <leononirac1992@gmail.com>
2020-06-09 11:41:10 +0000

    6/09 | started apm-speed application

---
+++ apm-speed/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './components/Header';
+export default class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
